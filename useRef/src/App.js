74133d3e47312b28ecc5719ddd5d843aff90ad7e
JayanthThoppil <48017821+JayanthThoppil@users.noreply.github.com>
2021-06-25 14:58:08 +0530

    adding useref hooks

---
+++ useRef/src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import React from "react";
+import FocusInput from "./FocusInput";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FocusInput />
+    </div>
+  );
+}
